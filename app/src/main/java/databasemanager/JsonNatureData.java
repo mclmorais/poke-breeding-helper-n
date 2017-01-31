262eec3e3650ea3c05539c01286ac900cf9053cb
Marcelo <marcelodotmorais@gmail.com>
2017-01-31 17:57:06 -0200

    Made initial JSON parsing interpreter for the Nature json data.

--- app/src/main/java/databasemanager/JsonNatureData.java
+++ app/src/main/java/databasemanager/JsonNatureData.java
@@ -1,8 +1,36 @@
 package databasemanager;
 
+import java.util.ArrayList;
+
 /**
- * Created by Marcelo on 30/01/2017.
+ * Contains nature retrieved from JSON files. This class receives its information
+ * from the JsonDataInterpreter class and provides it to the JsonDataProvider class.
+ *
+ * Negative values and empty string represent ERROR VALUES: filled data should never contain them.
+ * If the pokemon doesn't have a value (e.g. only has one type instead of two), is should be set to
+ * a "none" value instead of being left at -1.
  */
 
-public class JsonNatureData {
+class JsonNatureData {
+
+    ArrayList<JsonNatureDataBlock> data = new ArrayList<>();
+
+
+
+
+
+}
+
+class JsonNatureDataBlock {
+    private int id = -1;
+    private String name = "";
+    private int increased_stat_id = -1;
+    private int decreased_stat_id = -1;
+
+    JsonNatureDataBlock(int id, String name, int increased_stat_id, int decreased_stat_id) {
+        this.id = id;
+        this.name = name;
+        this.increased_stat_id = increased_stat_id;
+        this.decreased_stat_id = decreased_stat_id;
+    }
 }
--- app/src/main/java/marcelo/breguenait/breedinghelper/BreedingHelperApplication.java
+++ app/src/main/java/marcelo/breguenait/breedinghelper/BreedingHelperApplication.java
@@ -2,6 +2,7 @@ package marcelo.breguenait.breedinghelper;
 
 import android.app.Application;
 
+import databasemanager.JsonDataInterpreter;
 import databasemanager.JsonDataProvider;
 import databasemanager.SqlDatabase;
 
@@ -10,9 +11,9 @@ public class BreedingHelperApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        SqlDatabase.initialize(getApplicationContext());
-        JsonDataProvider.initialize(getApplicationContext());
-        //JsonDataProvider.initialize(getApplicationContext());
-        CachedPokemonIcons.initialize(getApplicationContext());
+        SqlDatabase.initialize(this);
+        JsonDataProvider.initialize(this);
+        CachedPokemonIcons.initialize(this);
+        JsonDataInterpreter jsonDataInterpreter = new JsonDataInterpreter(this);
     }
 }
